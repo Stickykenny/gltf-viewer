708ee2a8c4ca843b1f9427918616eebce89628be
Jeremy VONG <gnov.jeremy@gmail.com>
2023-12-11 10:51:29 +0100

    Implement glTF loading

--- src/ViewerApplication.hpp
+++ src/ViewerApplication.hpp
@@ -1,59 +1,86 @@
 #pragma once
 
+#include <tiny_gltf.h>
+
 #include "utils/GLFWHandle.hpp"
 #include "utils/cameras.hpp"
 #include "utils/filesystem.hpp"
 #include "utils/shaders.hpp"
+class ViewerApplication {
+   public:
+    ViewerApplication(const fs::path &appPath, uint32_t width, uint32_t height,
+                      const fs::path &gltfFile,
+                      const std::vector<float> &lookatArgs,
+                      const std::string &vertexShader,
+                      const std::string &fragmentShader,
+                      const fs::path &output);
 
-class ViewerApplication
-{
-public:
-  ViewerApplication(const fs::path &appPath, uint32_t width, uint32_t height,
-      const fs::path &gltfFile, const std::vector<float> &lookatArgs,
-      const std::string &vertexShader, const std::string &fragmentShader,
-      const fs::path &output);
+    bool loadGltfFile(tinygltf::Model &model) {
+        // using namespace tinygltf;
+        // tinygltf::Model model;
+        tinygltf::TinyGLTF loader;
+        std::string err;
+        std::string warn;
+        bool ret = loader.LoadASCIIFromFile(&model, &err, &warn,
+                                            m_gltfFilePath.string());
 
-  int run();
+        if (!warn.empty()) {
+            printf("Warn: %s\n", warn.c_str());
+        }
 
-private:
-  // A range of indices in a vector containing Vertex Array Objects
-  struct VaoRange
-  {
-    GLsizei begin; // Index of first element in vertexArrayObjects
-    GLsizei count; // Number of elements in range
-  };
+        if (!err.empty()) {
+            printf("Err: %s\n", err.c_str());
+        }
 
-  GLsizei m_nWindowWidth = 1280;
-  GLsizei m_nWindowHeight = 720;
+        if (!ret) {
+            printf("Failed to parse glTF\n");
+            return false;
+        }
+        return ret;
+    };
 
-  const fs::path m_AppPath;
-  const std::string m_AppName;
-  const fs::path m_ShadersRootPath;
+    int run();
 
-  fs::path m_gltfFilePath;
-  std::string m_vertexShader = "forward.vs.glsl";
-  std::string m_fragmentShader = "normals.fs.glsl";
+   private:
+    // A range of indices in a vector containing Vertex Array Objects
+    struct VaoRange {
+        GLsizei begin;  // Index of first element in vertexArrayObjects
+        GLsizei count;  // Number of elements in range
+    };
 
-  bool m_hasUserCamera = false;
-  Camera m_userCamera;
+    GLsizei m_nWindowWidth = 1280;
+    GLsizei m_nWindowHeight = 720;
 
-  fs::path m_OutputPath;
+    const fs::path m_AppPath;
+    const std::string m_AppName;
+    const fs::path m_ShadersRootPath;
 
-  // Order is important here, see comment below
-  const std::string m_ImGuiIniFilename;
-  // Last to be initialized, first to be destroyed:
-  GLFWHandle m_GLFWHandle{int(m_nWindowWidth), int(m_nWindowHeight),
-      "glTF Viewer",
-      m_OutputPath.empty()}; // show the window only if m_OutputPath is empty
-  /*
-    ! THE ORDER OF DECLARATION OF MEMBER VARIABLES IS IMPORTANT !
-    - m_ImGuiIniFilename.c_str() will be used by ImGUI in ImGui::Shutdown, which
-    will be called in destructor of m_GLFWHandle. So we must declare
-    m_ImGuiIniFilename before m_GLFWHandle so that m_ImGuiIniFilename
-    destructor is called after.
-    - m_GLFWHandle must be declared before the creation of any object managing
-    OpenGL resources (e.g. GLProgram, GLShader) because it is responsible for
-    the creation of a GLFW windows and thus a GL context which must exists
-    before most of OpenGL function calls.
-  */
+    fs::path m_gltfFilePath;
+    std::string m_vertexShader = "forward.vs.glsl";
+    std::string m_fragmentShader = "normals.fs.glsl";
+
+    bool m_hasUserCamera = false;
+    Camera m_userCamera;
+
+    fs::path m_OutputPath;
+
+    // Order is important here, see comment below
+    const std::string m_ImGuiIniFilename;
+    // Last to be initialized, first to be destroyed:
+    GLFWHandle m_GLFWHandle{int(m_nWindowWidth), int(m_nWindowHeight),
+                            "glTF Viewer", m_OutputPath.empty()};
+
+    // show the window only if m_OutputPath is empty
+    /*
+      ! THE ORDER OF DECLARATION OF MEMBER VARIABLES IS IMPORTANT !
+      - m_ImGuiIniFilename.c_str() will be used by ImGUI in ImGui::Shutdown,
+      which                             will be called in destructor of
+      m_GLFWHandle. So we must declare m_ImGuiIniFilename before m_GLFWHandle so
+      that m_ImGuiIniFilename                             destructor is called
+      after.
+      - m_GLFWHandle must be declared before the creation of any object managing
+      OpenGL resources (e.g. GLProgram, GLShader) because it is responsible for
+      the creation of a GLFW windows and thus a GL context which must exists
+      before most of OpenGL function calls.
+    */
 };
